5c06068f94768fcc587bb6834e4f5fb3c5eef093
Sujay jirapure <110679753+sujayjirapure@users.noreply.github.com>
2023-03-19 00:26:14 +0530

    done5

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Link, NavLink, Switch,Route } from "react-router-dom";
+import React from "react";
+import { Link, NavLink, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import PostDetail from "./PostDetail";
 import PostList from "./PostList";
 
-
 export default function Blog() {
-    return (
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/posts">Posts</NavLink>
-        </nav>
-  
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route exact path="/posts" component={PostList} />
-          <Route path="/posts/:id" component={PostDetail} />
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        {/* Create NavLink to About and Posts */}
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+      </nav>
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* Create Routes to About, PostList and PostDetail */}
+        <Route exact path="/about" component={About} />
+        <Route exact path="/posts" component={PostList} />
+        <Route exact path="/posts/:id" component={PostDetail} />
+      </Switch>
+    </div>
+
+
+
+  );
+}
