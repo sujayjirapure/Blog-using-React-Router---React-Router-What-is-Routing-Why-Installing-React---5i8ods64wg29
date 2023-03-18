7a091cea1c4bd15836b210d6ae0ae128646a1a94
Sujay jirapure <110679753+sujayjirapure@users.noreply.github.com>
2023-03-19 00:27:41 +0530

    edww

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import PostDetail from "./PostDetail";
 
 const posts = [
-    { name: 'Post 1', path: '/posts/1' },
-    { name: 'Post 2', path: '/posts/2' },
-    { name: 'Post 3', path: '/posts/3' },
-    { name: 'Post 4', path: '/posts/4' }
+  { name: "Post 1", path: "/posts/1" },
+  { name: "Post 2", path: "/posts/2" },
+  { name: "Post 3", path: "/posts/3" },
+  { name: "Post 4", path: "/posts/4" },
 ];
 export default function PostList() {
-    return (
-      <div className="posts">
-        <h1>Post List</h1>
-        <ul>
-        {items.map(item => (
-            <li key={item.name}>
-              <Link to={item.path}>{item.name}</Link>
-            </li>
-        ))}
-        </ul>
-      </div>
-    )
-  }
+  return (
+    <div className="posts">
+      <h1>Post List</h1>
+      <ul>
+        {/* Render a list of Links from the posts object   */}
+        {posts.map((e, i) => {
+          return (
+            <Link to={e.path} key={i}>
+              <li>{e.name}</li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
